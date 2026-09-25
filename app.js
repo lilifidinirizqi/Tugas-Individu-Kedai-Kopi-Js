813d1285e69e5fa41acd5286bf2ffb9dd3c26ea2
lilifidinirizqi <lilifidinirizqi@student.upi.edu>
2026-09-25 13:26:02 +0700

    feat: perulangan array menu rekomendasi ke console

--- app.js
+++ app.js
@@ -211,17 +211,25 @@ console.log("Tier Member:", tierMemberC);
 // TODO 6A:
 // Buat variabel Array bernama "menuRekomendasi" yang berisi minimal 5 nama menu kopi/makanan.
 
-
-
+const menuRekomendasi = [
+    "Caramel Macchiato",
+    "Kopi Susu Gula Aren",
+    "Croissant Butter Keju",
+    "Matcha Cream Latte",
+    "Cinnamon Roll Hangat"
+];
 
 // TODO 6B:
 // Gunakan perulangan "for loop" untuk mencetak setiap menu ke Console dengan format:
 // "1. Nama Menu", "2. Nama Menu", dst. Gunakan (i + 1) untuk nomor urutnya.
 
-
-
+for (let i = 0; i < menuRekomendasi.length; i++) {
+    console.log((i + 1) + ". " + menuRekomendasi[i]);
+}
 
 // TODO 6C:
 // Cetak jumlah total menu di akhir daftar menggunakan properti ".length".
 // Akhiri program dengan: console.log("=== TUGAS MANDIRI SELESAI DENGAN SUKSES! ===");
 
+console.log("Jumlah Menu:", menuRekomendasi.length);
+console.log("=== TUGAS MANDIRI SELESAI DENGAN SUKSES! ===");
